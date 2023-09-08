423ff79fac9c2c9cbc047e4557f6b8292d73b8e3
Nyaradzo <nmanyange30@gmail.com>
2023-09-08 11:53:19 -0700

    Anything positive is better

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,9 +14,9 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 		printf("%d is positive", n);
-	else if (n < 0)
-		printf("%d is negative", n);
-	else
+	else if (n == 0)
 		printf("%d is zero", n);
+	else
+		printf("%d is negative", n);
 	return (0);
 }
