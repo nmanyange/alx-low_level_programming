4abdd9fcdccefccca768a2188b4a4e3ed1d17e71
Nyaradzo <nmanyange30@gmail.com>
2023-09-09 00:05:21 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print lowercase alphabet in reverse
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	char i;
+
+	for (i = 'z' ; i > 'a' ; i--)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
