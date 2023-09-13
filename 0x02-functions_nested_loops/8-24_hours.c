cdc74c87aef3aee85fd64fc20c7efcb41dd8674f
Nyaradzo <nmanyange30@gmail.com>
2023-09-12 22:43:57 -0700

    I am federal agent

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,22 +5,18 @@
  */
 void jack_bauer(void)
 {
-	int i, j;
+	int hour, minute;
 
-	i = 0;
-	while (1 < 24)
+	for (hour = 0; hour <= 23; hour++)
 	{
-		j = 0;
-		while (j < 60)
+		for (minute = 0; minute <= 59; minute++)
 		{
-			_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
 			_putchar(':');
-			_putchar((j / 10) + '0');
-			_putchar((j % 10) + '0');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
 			_putchar('\n');
-			j++;
 		}
-		i++;
-		return (0);
 	}
+}
