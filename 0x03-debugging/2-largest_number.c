3eda62718745aa773261bfece38c05ed1945b746
Nyaradzo <nmanyange30@gmail.com>
2023-09-15 08:54:24 -0700

    0972

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,17 +10,17 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && a > c)
+	if (a >= b && a >= c)
 	{
 		largest = a;
 	}
-	else if (a > b && c > a)
-	{
-		largest = c;
-	}
-	else if (b > c)
+	if (b >= a && b >= c)
 	{
 		largest = b;
+	}
+	else
+	{
+		largest = c;
 	}
 	return (largest);
 }
