9b603d092656a9640f9c7718abf8494e46f3b224
Nyaradzo <nmanyange30@gmail.com>
2023-10-02 04:44:58 -0700

    A library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strncat - concatenates two strings
+ * @dest: destination string
+ * @src: source string
+ * @n: bytes of the string
+ * Return: a pointer to the resulting string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int dest_len, i;
+
+	for (dest_len = 0; dest[dest_len] != '\0'; dest_len++)
+		;
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[dest_len + i] = src[i];
+	dest[dest_len + i] = '\0';
+	return (dest);
+}
