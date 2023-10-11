c709dd3248fd407275d329b371807be7d3de071e
Nyaradzo <nmanyange30@gmail.com>
2023-10-11 12:30:57 -0700

    a function that performs simple operations

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -18,11 +18,11 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int i;
+	int i = 0;
 
 	while (ops[i].op)
 	{
-		if (*s == *(ops[i].op) && s[1] == '/0')
+		if (*s == *(ops[i].op) && s[1] == '\0')
 		{
 			return (ops[i].f);
 		}
