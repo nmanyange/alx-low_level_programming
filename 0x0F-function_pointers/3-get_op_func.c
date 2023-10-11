e108e8a77ee13ee9688c354480104f98e4d65723
Nyaradzo <nmanyange30@gmail.com>
2023-10-11 06:23:21 -0700

    a program that performs simple operations

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,33 @@
+
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * get_op_func - gets an operator function
+ * @s: operator
+ * Return: 0
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+	int i;
+
+	while (ops[i].op)
+	{
+		if (*s == *(ops[i].op) && s[1] == '\0')
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - check code
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+	char *operator;
+	int(*op_func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+	num1 = atoi(argv[1]);
+	operator = argv[2];
+	num2 = atoi(argv[3]);
+
+	op_func = get_op_func(operator);
+
+	if ((op_func == NULL) || operator[1] != '\0')
+	{
+		printf("Error\n");
+		return (99);
+	}
+	if ((*operator == '/') || (*operator == '%' && num2 == 0))
+	{
+		printf("Error\n");
+		return (100);
+	}
+	result = op_func(num1, num2);
+	printf("%d\n", result);
+	return (0);
+}
