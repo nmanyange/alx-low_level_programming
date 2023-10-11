5718cef2c3be59b4d654896b7b4a07583263598c
Nyaradzo <nmanyange30@gmail.com>
2023-10-11 10:09:17 -0700

    a program that performs simple operations

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -20,9 +20,13 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i;
 
-	i = 0;
-
-	while (ops[i].op != NULL && *(ops[i].op != *s)
-			i++;
-		return (ops[i].f);
+	while (ops[i].op)
+	{
+		if (*s == *(ops[i].op) && s[1] == '/0')
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	return (NULL);
 }
