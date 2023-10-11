affa0f81834186aed66b2146ab61ae7b2a8f2977
Nyaradzo <nmanyange30@gmail.com>
2023-10-11 09:15:20 -0700

    a program that performs simple operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -31,7 +31,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (99);
 	}
-	if ((*operator == '/') || (*operator == '%' && num2 == 0))
+	if ((*operator == '/' && num2 == 0) || (*operator == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		return (100);
