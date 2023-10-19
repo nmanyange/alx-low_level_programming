53562a9eb533a95925ce3555bc32b40cfeedf2ac
Nyaradzo <nmanyange30@gmail.com>
2023-10-19 07:31:28 -0700

    list length

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -5,9 +5,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-
 /**
  * struct list_s - singly linked list
  * @str: string
@@ -21,5 +18,8 @@ typedef struct list_s
 	unsigned int len;
 	struct list_s *next;
 } list_t;
+
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
 
 #endif
