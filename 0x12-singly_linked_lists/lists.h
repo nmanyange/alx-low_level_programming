b759fcb5b82d2298fed261f7ec7b16813099036c
Nyaradzo <nmanyange30@gmail.com>
2023-10-19 07:09:58 -0700

    list length

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,25 @@
+#ifndef _LIST_H_
+#define _LIST_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+
+/**
+ * struct list_s - singly linked list
+ * @str: string
+ * len: length of the string
+ * @next: pointer to the next node
+ */
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+#endif
