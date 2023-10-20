0ab87e45e52f28e4d60824ab6f8b7efdb929965b
Nyaradzo <nmanyange30@gmail.com>
2023-10-20 10:21:53 -0700

    the hare and the tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+void ComeFirstbyNM(void)__attribute__((constructor));
+/**
+ * ComeFirst - function that runs befire the main function
+ */
+void ComeFirstbyNM(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
